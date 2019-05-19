d09564c72cfdd0535bb2b56a3e9c01989ff6a337
AmmarAlkhooly98 <ammaralkhooly1@gmail.com>
2019-05-20 01:08:36 +0300

    fetched from database to client with filter

--- database/models.js
+++ database/models.js
@@ -10,24 +10,24 @@ const engineer = db.define('engineer', {
 });
 
 const worker = db.define('worker', {
-    id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-    fullName: { type: Sequelize.STRING, required: true },
-    userName: { type: Sequelize.STRING, required: true, unique: true },
-    password: { type: Sequelize.STRING, required: true },
-    experienceLevel: { type: Sequelize.STRING },
-    expectedSalary: { type: Sequelize.INTEGER, required: true },
-    phoneNumber: { type: Sequelize.INTEGER, required: true },
-    status: { type: Sequelize.INTEGER },
-    role: { type: Sequelize.STRING, required: true }
+	id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+	fullName: { type: Sequelize.STRING, required: true },
+	userName: { type: Sequelize.STRING, required: true, unique: true },
+	password: { type: Sequelize.STRING, required: true },
+	experienceLevel: { type: Sequelize.STRING },
+	expectedSalary: { type: Sequelize.INTEGER, required: true },
+	phoneNumber: { type: Sequelize.INTEGER, required: true },
+	status: { type: Sequelize.STRING },
+	role: { type: Sequelize.STRING, required: true }
 });
 
 const order = db.define('order', {
-    id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-    engineerName: { type: Sequelize.STRING, required: true },
-    workerName: { type: Sequelize.STRING, required: true },
-    endDate: { type: Sequelize.DATE },
-    status: { type: Sequelize.INTEGER },
-    feedBack: { type: Sequelize.STRING, required: false }
+	id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+	engineerName: { type: Sequelize.STRING, required: true },
+	workerName: { type: Sequelize.STRING, required: true },
+	endDate: { type: Sequelize.DATE },
+	status: { type: Sequelize.INTEGER },
+	feedBack: { type: Sequelize.STRING, required: false }
 });
 
 engineer.belongsToMany(worker, { through: order });
