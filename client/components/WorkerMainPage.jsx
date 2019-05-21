e196119b9aa0f42d31f98d36cab83a1064136787
AmmarAlkhooly98 <ammaralkhooly1@gmail.com>
2019-05-21 23:50:14 +0300

    client updates

---
+++ client/components/WorkerMainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link, NavLink, Redirect } from 'react-router-dom';
+
+class WorkerMainPage extends React.Component {
+	render() {
+		return (
+			<div>
+				<h2> Workers:</h2>
+			</div>
+		);
+	}
+}
+
+export default WorkerMainPage;
