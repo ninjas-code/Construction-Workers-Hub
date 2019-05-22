607b434d99dc8452179c9a59c45fc9b0ac3d56f4
Abdulrohman-Alakrad <Abdulrohman20153091@gmail.com>
2019-05-22 22:41:01 +0300

    pull new Changes

--- client/components/Profiles.jsx
+++ client/components/Profiles.jsx
@@ -5,24 +5,10 @@ class Profiles extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-<<<<<<< HEAD
-				userProfile: [],
-				show:false
-				
-			};
-		}
-		clicked(){
-			this.setState({
-				show : !this.state.show
-			})
-		}
-	componentDidMount(){
-=======
 				userProfile: [] 
 			};
 		}
 	componentWillMount(){
->>>>>>> d74b71247760f509c631d99f788e63293025ca34
 		 let that = this;
 		
 		const  { match }  = this.props;
@@ -38,36 +24,6 @@ class Profiles extends React.Component {
 
 	 
 	 	render() {
-<<<<<<< HEAD
-			//const  { params }  = this.props;
-			// var x= this.state.userProfile;
-			// var name = x[0];
-			// console.log(name);
-			// console.log(x[0])
-			
-			return (
-		    <div>
-				<h1>THE PROFILE OF {this.state.userProfile.fullName}YOU CHOOSED</h1>
-		    <table> 
-		     <tbody>
-			{this.state.userProfile.map( (user, i) => 
-            <tr key = {i } style={{  margin: '10px', display:'block' }}>  
-			<td><strong> NAME: </strong></td><td>{user.fullName}</td>
-			<td><strong>EXPERIENCE LEVEL:</strong></td><td>{user.experienceLevel}</td>
-			<td><strong>SALARY:</strong></td><td>{user.expectedSalary}</td>
-			<td><strong>PHONE NUMBER:</strong></td><td>{user.phoneNumber}</td>
-			<td><strong>STATUS:</strong></td><td>{user.status}</td>
-			<td><strong>ROLE:</strong></td><td>{user.role}</td>
-		   </tr>
-		  )}
-		 </tbody>
-		 
-		  
-		</table>
-		<button onClick={this.clicked.bind(this)}>Book Worker</button><br/><br/>
-				{this.state.show===true ? <input type='text' placeholder='putdate'/> : null }
-	 </div>	
-=======
 			return (
 		    <div>
 					{ this.state.userProfile.map( (user, i)=>
@@ -106,7 +62,6 @@ class Profiles extends React.Component {
 				
 			 
 	      </div>	
->>>>>>> d74b71247760f509c631d99f788e63293025ca34
 
 		);
 	}
