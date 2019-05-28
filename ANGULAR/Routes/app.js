813666ad9a92914015b047a4eb80db5a8bcc8264
RaedShihab <raedshihab92@gmail.com>
2019-05-28 13:02:36 +0300

    routes is working

---
+++ ANGULAR/Routes/app.js
@@ -0,0 +1,14 @@
+
+    var app = angular.module("app", ["ngRoute"]);
+    app.config(function($routeProvider) {
+      $routeProvider
+        .when("/signup", {
+          templateUrl: "./veiw/signup.html",
+          controller: "ctrl1"
+        })
+        .when("/item2", {
+          templateUrl: "./veiw/item2.html",
+          controller: "ctrl2"
+        })
+
+    });
