50e9196ad51ddb01e5fcea138f8c0c0d32e58079
Qusai <qusaiznemat6@gmail.com>
2019-05-29 13:46:15 +0300

    NewDataBaseOnline

--- database/db.js
+++ database/db.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var Sequelize = require('sequelize');
 
 //Database connection
-const db = new Sequelize('sql2292452', 'sql2292452', 'cV9!kH3%', {
-	host: 'sql2.freesqldatabase.com',
+const db = new Sequelize('sql7293816', 'sql7293816', 'iDWuS81vjK', {
+	host: 'sql7.freesqldatabase.com',
 	dialect: 'mysql'
 });
 
