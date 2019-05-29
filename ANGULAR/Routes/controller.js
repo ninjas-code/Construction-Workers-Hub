51b8012f5ec5616af2d58e35ded8690fc74a412f
RaedShihab <raedshihab92@gmail.com>
2019-05-30 00:05:48 +0300

    wrking on signin1

--- ANGULAR/Routes/controller.js
+++ ANGULAR/Routes/controller.js
@@ -1,4 +1,5 @@
 app.controller("ctrl2", function($scope,  $http) {
+  $scope.var1='Raed'
     console.log("hhhhhhh")
     $scope.sendWorkerDataToServer= function(fullname, username, password, experiencelevel, expectedsalary, phonenumber, role, status) {
       // console.log(fullname);
@@ -24,6 +25,7 @@ app.controller("ctrl2", function($scope,  $http) {
     }
   });
 app.controller("ctrl1", function($scope,  $http) {
+  $scope.var2='Shihab'
     $scope.uploader = function(files) {
       console.log(files)
     //   $http.post('upload.ashx', $scope.files,
@@ -60,32 +62,32 @@ app.controller("ctrl1", function($scope,  $http) {
     
   });
 
+  app.controller("signin", function($scope,  $http) {
+    $scope.signinAsAnEngineer= function(userName, Password) {
+      console.log(userName, Password)
+      var body = {
+        username: userName,
+        password: Password
+      }
+      $http({
+        method: 'POST',
+        headers:{"Content-Type" : "application/json"},
+        body :JSON.stringify(body),
+        url: 'http://localhost:5002/signinEngineer'
+      }).then(function successCallback(response) {
+        console.log(response)
+        return response.json();
+          // this callback will be called asynchronously
+          // when the response is available
+        }).then(function(data) {
+          console.log( data);
+        });
+    }
+  });
 
 
-//   app.controller('myCtrl3', function($scope ,$http) {
-//     $scope.SendSingInWorker= function(Username ,Password){
-//       console.log(Username , Password)
-//        var body = {
-//          username: Username,
-//          password:Password
-//        }
-     
-//        $http({
-//         method: 'POST',
-//         headers:{"Content-Type" : "application/json"},
-//         body :JSON.stringify(body),
-//         url: 'http://localhost:5000/signinWorker'
-//       }).then(function successCallback(response) {
-//         console.log(response)
-//         return response.json();
-//           // this callback will be called asynchronously
-//           // when the response is available
-//         }).then(function(data) {
-//           console.log( data);
-//         });
-//     }
-//  });
 
+  
   // app.post('/signinWorker', function(req, res) {
   //   const username = req.body.username;
   //   const password = req.body.password;
@@ -117,7 +119,7 @@ $http({
     // $scope.Professionl = "Professional	";
     // $scope.Status = "Active";
     // $scope.Role = "Carpenter";
-    
+    $scope.var3="Hello"
   });
 
 
